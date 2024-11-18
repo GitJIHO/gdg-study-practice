2a6a19ebde54a816cdec2cf165824a200eb3fd48
gitjiho <jiho99322@gmail.com>
2024-11-18 20:39:51 +0900

    test: test IndexControllerTest

--- src/test/java/com/amazon/practice/PracticeApplicationTests.java
+++ src/test/java/com/amazon/practice/PracticeApplicationTests.java
@@ -2,12 +2,23 @@ package com.amazon.practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class PracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public TestRestTemplate testRestTemplate() {
+            return new TestRestTemplate();
+        }
+    }
 
 }
