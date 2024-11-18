511d7ebd8c0f141c3e214c68fb39439073c6e0a4
gitjiho <jiho99322@gmail.com>
2024-11-18 23:55:00 +0900

    feat: 게시판 글 수정, 삭제 추가

--- src/main/java/com/amazon/practice/web/controller/IndexController.java
+++ src/main/java/com/amazon/practice/web/controller/IndexController.java
@@ -1,9 +1,11 @@
 package com.amazon.practice.web.controller;
 
 import com.amazon.practice.domain.service.PostsService;
+import com.amazon.practice.web.dto.PostsOnlyResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -23,5 +25,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsOnlyResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
--- src/main/java/com/amazon/practice/web/dto/PostsOnlyResponseDto.java
+++ src/main/java/com/amazon/practice/web/dto/PostsOnlyResponseDto.java
@@ -2,7 +2,26 @@ package com.amazon.practice.web.dto;
 
 import com.amazon.practice.domain.posts.Posts;
 
-public record PostsOnlyResponseDto(
-        Posts posts
-) {
+public class PostsOnlyResponseDto {
+    private final Posts posts;
+
+    public PostsOnlyResponseDto(Posts posts) {
+        this.posts = posts;
+    }
+
+    public Long getId() {
+        return posts.getId();
+    }
+
+    public String getTitle() {
+        return posts.getTitle();
+    }
+
+    public String getContent() {
+        return posts.getContent();
+    }
+
+    public String getAuthor() {
+        return posts.getAuthor();
+    }
 }
